8ff6da81d01b37ce43137b4be0a88a5a7f6a3285
Ken Crocken <kcrocken@gmail.com>
2015-09-02 01:32:06 -0400

    Fixes wax wane boolean, adds comments, et al

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,44 +1,59 @@
 'use strict';
 
 angular.module('lunarPhases')
-  .controller('MainCtrl', function ($scope, $geolocation, Astronomy) {
-    $scope.loadingDone = false;
+    .controller('MainCtrl', function ($scope, $geolocation, Astronomy) {
+        $scope.loadingDone = false;
 
-    $scope.getData = function(data){
-      $scope.myPosition = data;
+        //Gets the astronomy data
+        $scope.getData = function(data){
 
-      var astronomy = Astronomy.get({lat: $scope.myPosition.coords.latitude, long: $scope.myPosition.coords.longitude});
-      astronomy.$promise.then(function(data){
+            //Store geolocation data 
+            $scope.myPosition = data;
 
-        $scope.lunarPhase = data.moon_phase;
-        $scope.lunarPhase.percentIlluminated = String($scope.lunarPhase.percentIlluminated * 0.01);
-        
-        //Waxing is true, Waning is false
-        var x = angular.lowercase($scope.lunarPhase.phaseofMoon);
-        $scope.phaseBool = x.indexOf('wax') != -1 ? true : false;
+            //Call the factory and get the astronomy location
+            var astronomy = Astronomy.get({lat: $scope.myPosition.coords.latitude, long: $scope.myPosition.coords.longitude});
+            astronomy.$promise.then(function(data){
 
-      });
-    };
+                //Store moon phase data
+                $scope.lunarPhase = data.moon_phase;
+                //Convert percent 
+                $scope.lunarPhase.percentIlluminated = String($scope.lunarPhase.percentIlluminated * 0.01);
+                
+                //Waxing is true, Waning is false
+                var x = angular.lowercase($scope.lunarPhase.phaseofMoon);
+                $scope.phaseBool = x.indexOf('wax') != -1 ? true : false;
 
-    $geolocation.getCurrentPosition({
-      timeout: 4000
-    }).then(function(position) {
+            });
+        };
 
-      $scope.loadingDone = true;
-      $scope.getData(position);
-      console.log(position);
-    }, function(error){
-      $scope.loadingDone = true;
-      $scope.error = error;
-      console.log($scope.error);
-      var defaultPosition = {
-        coords: {
-          latitude: 39.101269,
-          longitude: -76.8514
-        }
-      };
-      console.log(defaultPosition);
-      $scope.getData(defaultPosition);
+        //Gets geolocation data
+        $geolocation.getCurrentPosition({
+            timeout: 4000
+        }).then(function(position) {
+
+            //If successful
+            $scope.loadingDone = true;
+            //Pass geolocation data to get astronomy
+            $scope.getData(position);
+
+        }, function(error){
+
+            //If error
+            $scope.loadingDone = true;
+
+            //Save error message
+            $scope.error = error;
+
+            // Set default
+            var defaultPosition = {
+                coords: {
+                    latitude: 39.101269,
+                    longitude: -76.8514
+                }
+            };
+
+            //Pass default location to get astronomy
+            $scope.getData(defaultPosition);
+        });
+
     });
-
-  });
--- src/app/moonPhase.js
+++ src/app/moonPhase.js
@@ -13,8 +13,14 @@ angular.module('lunarPhases')
             scope.percent = attr.percentIllum;
             scope.bool = attr.waxWane;
 
+            //Function to draw the moon phase
             scope.draw = function(){
-                drawPlanetPhase(document.getElementById('moon'), scope.percent, scope.bool, {
+                
+                //Converts string to a boolean
+                var bool = (scope.bool == "true");
+
+                //Invokes the planetPhase.js 
+                drawPlanetPhase(document.getElementById('moon'), scope.percent, bool, {
                     diameter: attr.diameter,
                     shadowColour: attr.shadowColour,
                     lightColour: attr.lightColour,
@@ -23,24 +29,24 @@ angular.module('lunarPhases')
                 });
             };
 
+            //Looks for changes
             attr.$observe('percentIllum', function(value){
                 if(value){
                     scope.percent = value;
-                    // console.log(value);
-                    // scope.draw();
                 }
             });
+
             attr.$observe('waxWane', function(value){
                 if(value){
                     scope.bool = value;
-                    // console.log(value);
-                    // scope.draw();
                 }
             });
+
+            //When there is a new value, calls the draw function - allows
+            //data to be returned to controller
             scope.$watchGroup(['percent','bool'], function(newValues, oldValues){
                 if(newValues[1] != ""){
                     scope.draw();
-                    // console.log(newValues);
                 }
             });
         }
