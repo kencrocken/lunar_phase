fdfa2c21ef36b45ec98a2e67a15178df5125eaec
Ken Crocken <kcrocken@gmail.com>
2024-05-25 00:24:03 -0400

    refactor: Update moon shadow and light colors

--- src/Moon/moon.tsx
+++ src/Moon/moon.tsx
@@ -3,8 +3,8 @@ import type { NavalMoonAPI } from '../navalApi.types';
 
 const MOON_DEFAULTS = {
   diameter: 200,
-  shadowColor: "rgb(230, 230, 230)",
-  lightColor: "rgb(51, 51, 51)",
+  shadowColor: "rgb(51, 51, 51)",
+  lightColor: "rgb(230, 230, 230)",
   earthshine: 0.1,
   blur: 3
 }
