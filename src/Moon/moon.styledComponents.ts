4367b84defa3c292142016601f04f3612a8cc076
Ken Crocken <kcrocken@gmail.com>
2024-05-25 13:00:31 -0400

    refactor: Update moon component styles and structure

--- src/Moon/moon.styledComponents.ts
+++ src/Moon/moon.styledComponents.ts
@@ -12,10 +12,12 @@ const OuterMoon = styled.div<{ $outerDiameter: number, $outerColor: string }>`
   z-index: 1000;
   overflow: hidden;
   transform: translateX(-50%);
-  height: ${(props) => props.$outerDiameter}px;
-  width: ${(props) => props.$outerDiameter}px;
-  background-color: ${(props) => props.$outerColor};
-  border-radius: ${(props) => props.$outerDiameter/2}px;
+  ${({ $outerDiameter, $outerColor }) => `
+    height: ${$outerDiameter}px;
+    width: ${$outerDiameter}px;
+    background-color: ${$outerColor};
+    border-radius: ${$outerDiameter/2}px;
+  `}
 `;
 
 const InnerMoon = styled.div<{ 
@@ -28,14 +30,16 @@ const InnerMoon = styled.div<{
   $earthshineDefault: number
 }>`
   position: absolute;
-  background-color: ${(props) =>props.$innerColor};
-  border-radius: ${(props) => props.$blurredDiameter/2}px;
-  height: ${(props) => props.$blurredDiameter}px;
-  width: ${(props) =>  props.$blurredDiameter}px;
-  left: ${(props) =>  props.$blurredOffset}px;
-  top: ${(props) => (props.$moonDiameter-props.$blurredDiameter)/2}px;
-  box-shadow: 0px 0px ${({$blurDefault}) => $blurDefault }px ${({$blurDefault}) => $blurDefault }px ${({$innerColor}) => $innerColor };
-  opacity: ${(props) => 1 - props.$earthshineDefault};
+  ${({ $moonDiameter, $innerDiameter, $blurredDiameter, $blurredOffset, $innerColor, $blurDefault, $earthshineDefault }) => `
+    background-color: ${$innerColor};
+    border-radius: ${$blurredDiameter/2}px;
+    height: ${$blurredDiameter}px;
+    width: ${$blurredDiameter}px;
+    left: ${$blurredOffset}px;
+    top: ${($moonDiameter-$blurredDiameter)/2}px;
+    box-shadow: 0px 0px ${$blurDefault}px ${$blurDefault}px ${$innerColor};
+    opacity: ${1 - $earthshineDefault};
+  `}
 `;
 
 export { MoonWrapper, OuterMoon, InnerMoon };
