40f9778a0d52036b192e2803ec4bb0b0a418e415
Ken Crocken <kcrocken@gmail.com>
2024-06-06 14:11:36 -0400

    refactor: Update useMoonData to use fetchMoonData service function

--- src/Hooks/useMoonData.ts
+++ src/Hooks/useMoonData.ts
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+
+import { fetchMoonData } from '../service/moonData.service';
+
 import type { Location } from "../moonApi.types";
 import type { NavalMoonAPI } from "../navalApi.types";
 
@@ -7,29 +10,24 @@ export const useMoonData = (currentDate: string, coords?: Location) => {
   const [moonData, setMoonData] = useState<NavalMoonAPI>();
   const [error, setError] = useState<string>();
   
-  const url = 'https://ald76uu0g6.execute-api.us-east-1.amazonaws.com/project/lunarphase';
+
   
   useEffect(() => {
     const options = {
-      method: 'GET',
-      headers: {
-        'latitude': `${coords?.latitude}` || '',
-        'longitude': `${coords?.longitude}` || '',
-        'current-date': currentDate,
-      },
+      'latitude': `${coords?.latitude}` || '',
+      'longitude': `${coords?.longitude}` || '',
+      'current-date': currentDate,
     };
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        setMoonData(result); 
+        const result = await fetchMoonData(options);
+        setMoonData(result);
         setFetchingData(false);
       } catch (error: any) {
-        console.error(error);
-        setError(error.message as string);
+        setError(error.message);
         setFetchingData(false);
-      } 
+      }
     };
 
     if (coords !== undefined && currentDate){
---
+++ src/service/moonData.service.ts
@@ -0,0 +1,28 @@
+import type { NavalMoonAPI } from '../navalApi.types';
+
+const url = 'https://ald76uu0g6.execute-api.us-east-1.amazonaws.com/project/lunarphase';
+
+type MoonDataFetchOptions = {
+  latitude: string;
+  longitude: string;
+  'current-date': string;
+};
+
+export const fetchMoonData = async (requestParams: MoonDataFetchOptions): Promise<NavalMoonAPI> => {
+  const options = {
+    method: 'GET',
+    headers: { 
+      'Content-Type': 'application/json',
+      ...requestParams 
+    }
+  }
+
+  try {
+    const response = await fetch(url, options);
+    const result: NavalMoonAPI = await response.json();
+    return result;
+  } catch (error: any) {
+    console.error(error);
+    throw new Error(error.message as string);
+  }
+};
